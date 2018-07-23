77f5da7dadcb6fde8a1d48a25890775fddcc3212
Chris Myers <chris.myers@rheem.com>
2018-07-23 12:06:46 -0400

    coordinator works with basic presentation of container view.

---
+++ ContainerCoordinator/TopView.swift
@@ -0,0 +1,21 @@
+//
+//  TopView.swift
+//  ContainerCoordinator
+//
+//  Created by Chris Myers on 7/23/18.
+//  Copyright © 2018 CM. All rights reserved.
+//
+
+import UIKit
+
+class TopView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
